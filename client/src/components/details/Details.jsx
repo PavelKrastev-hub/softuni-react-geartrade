6060ab50e6291b83cdfc0a05c81d3069382b93a9
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-11-27 20:47:17 +0200

    Create delete handler

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { BASE_URL } from "../../utils/constants.js";
 
 export default function Details() {
@@ -8,12 +8,32 @@ export default function Details() {
 
     const price = Number(part.price).toFixed(2);
 
+    const navigate = useNavigate();
+
     useEffect(() => {
         fetch(`${BASE_URL}/${partId}`)
             .then(response => response.json())
             .then(result => setPart(result))
             .catch(err => alert(err.message));
     }, [partId]);
+
+    const deleteGameHandler = async () => {
+        const isConfirmed = confirm(`Are you sure you want to delete your part - ${part.name}`);
+
+        if (!isConfirmed) {
+            return;
+        }
+        try {
+
+            await fetch(`${BASE_URL}/${partId}`, {
+                method: 'DELETE'
+            })
+            navigate('/parts');
+        } catch (err) {
+            (alert(err.message));
+        }
+    }
+    
     return (
         <section className="min-h-[calc(100vh-112px)] bg-[url('/images/carParts.jpg')] bg-cover bg-center py-40">
             <div className="max-w-7xl mx-auto p-1 rounded-5xl bg-gradient-to-r from-black via-red-500 to-blue-500 transform">
@@ -60,6 +80,22 @@ export default function Details() {
                             <p className="text-3xl font-bold text-red-600 mt-4 border-t-2 border-gray-200 pt-4">
                                 ${price}
                             </p>
+                        </div>
+                        <div className="mt-6 flex justify-end space-x-4">
+                            <button
+                                className="relative px-6 py-3 bg-white border-4 border-black rounded-lg font-bold text-lg uppercase tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 shadow-md hover:scale-105 hover:shadow-lg transition-transform transition-shadow"
+                            >
+                                Edit Part
+                                <span className="absolute inset-0 rounded-lg border-2 border-black pointer-events-none"></span>
+                            </button>
+
+                            <button
+                                className="relative px-6 py-3 bg-white border-4 border-black rounded-lg font-bold text-lg uppercase tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 shadow-md hover:scale-105 hover:shadow-lg transition-transform transition-shadow"
+                                onClick={deleteGameHandler}
+                            >
+                                Delete Part
+                                <span className="absolute inset-0 rounded-lg border-2 border-black pointer-events-none"></span>
+                            </button>
                         </div>
                     </div>
                 </div>

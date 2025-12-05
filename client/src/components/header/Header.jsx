dd37547c504cb7728dbc29ad8f61997a11beca4c
PavelKrastev-hub <pavel__krustev@abv.bg>
2025-12-05 11:19:58 +0200

    Add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
+import { useUserContext } from "../../contexts/UserContext.jsx";
 
 export default function Header() {
+    const { isAuthenticated } = useUserContext();
+
     return (
         <header className="bg-gray-900 text-white">
             <nav className="max-w-9xl mx-auto px-6 py-4 flex items-center justify-between">
@@ -20,31 +23,42 @@ export default function Header() {
                             About Us
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/parts/create" className="hover:text-red-400">
-                            Create
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/login" className="hover:text-red-400">
-                            Login
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/register" className="hover:text-red-400">
-                            Register
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/parts/myOffers" className="hover:text-red-400">
-                            My Offers
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/logout" className="hover:text-red-400">
-                            Logout
-                        </Link>
-                    </li>
+                    {isAuthenticated
+                        ? (
+                            <>
+                                <li>
+                                    <Link to="/parts/create" className="hover:text-red-400">
+                                        Create Offer
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/parts/myOffers" className="hover:text-red-400">
+                                        My Offers
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/logout" className="hover:text-red-400">
+                                        Logout
+                                    </Link>
+                                </li>
+                            </>
+                        )
+                        : (
+                            <>
+                                <li>
+                                    <Link to="/login" className="hover:text-red-400">
+                                        Login
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/register" className="hover:text-red-400">
+                                        Register
+                                    </Link>
+                                </li>
+                            </>
+                        )
+                    }
+
                 </ul>
             </nav>
         </header>
